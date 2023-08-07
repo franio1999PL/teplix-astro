c6de958ee5d369e08d7ab31cc5d5006b42387374
Franciszek Sikora <sikorafranek@gmail.com>
2023-08-08 01:14:24 +0200

    updated navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,20 +9,38 @@ const Links: LinkType[] = [
     slug: '/projekty'
   },
   {
-    name: 'Testowy',
-    slug: '/test'
+    name: 'QrCode',
+    slug: '/qrcode'
+  },
+  {
+    name: 'QrCode',
+    slug: '/qrcode'
+  },
+  {
+    name: 'QrCode',
+    slug: '/qrcode'
+  },
+  {
+    name: 'QrCode',
+    slug: '/qrcode'
   }
 ]
 
 export default function Navbar () {
   return (
-    <nav className='bg-slate-900 text-white p-4'>
+    <nav className='m-8 bg-white p-4 text-black  '>
       <ul className='flex justify-around items-center'>
+        <img src='logo.png' className='w-1/8' alt='' />
         {Links.map(({ name, slug }) => (
           <li key={name}>
-            <a href={slug}>{name}</a>
+            <a className='text-base font-bold uppercase' href={slug}>
+              {name}
+            </a>
           </li>
         ))}
+        <li className='bg-[#00b83f] text-white p-4 text-md font-bold uppercase border-2 border-[#00b83f] hover:bg-white hover:text-[#00b83f] transition-all duration-300'>
+          <a href='#'>Kontakt</a>
+        </li>
       </ul>
     </nav>
   )
