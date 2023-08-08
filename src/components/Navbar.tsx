7a5e7523f01e1eec517177a024473298274db604
Franciszek Sikora <sikorafranek@gmail.com>
2023-08-08 22:35:17 +0200

    updated colors and added blog page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import LogoSvg from '/teplix-logo.svg'
+
 type LinkType = {
   name: string
   slug: string
@@ -14,29 +16,30 @@ const Links: LinkType[] = [
     slug: '/qrcode'
   },
   {
+    name: 'Projekty',
+    slug: '/projekty'
+  },
+  {
     name: 'Grzejniki Próżniowe',
     slug: 'https://grzejnikiprozniowe.pl',
     newTab: true
   },
   {
-    name: '(Wyłączony link)',
-    slug: '#'
-  },
-  {
-    name: '(Wyłączony link)',
-    slug: '#'
+    name: 'Blog',
+    slug: '/blog'
   }
 ]
 
 export default function Navbar () {
   return (
-    <nav className='m-8 bg-white p-8 text-black z-10 top-0 left-0 sticky  w-[96%] hover:drop-shadow-lg transition-all duration-300'>
+    <nav className='m-8 bg-white p-8 text-black z-10 top-0 left-0 sticky  w-[96%] hover:drop-shadow-md transition-all duration-300'>
       <ul className='flex justify-around items-center'>
-        <img src='logo.png' className='w-1/8' alt='' />
+        {/* <img src='logo.png' className='w-1/8' alt='' /> */}
+        <img src={LogoSvg} className='w-[248px]' alt='' />
         {Links.map(({ name, slug, newTab }) => (
           <li key={name}>
             <a
-              className='text-base font-bold uppercase hover:text-[#00b83f]'
+              className='text-base font-bold uppercase hover:text-[#583BD1]'
               href={slug}
               target={newTab ? '_blank' : ''}
             >
@@ -45,7 +48,7 @@ export default function Navbar () {
           </li>
         ))}
         <a href='/kontakt'>
-          <button className='bg-[#00b83f] text-white p-4 text-md font-bold uppercase border-2 border-[#00b83f] hover:bg-white hover:text-[#00b83f] transition-all duration-300 ease-in hover:-translate-y-1 hover:shadow-2xl '>
+          <button className='bg-[#583BD1] text-white p-4 text-md font-bold uppercase border-2 border-[#583BD1] hover:bg-white hover:text-[#583BD1] transition-all duration-300 ease-in hover:-translate-y-1 hover:shadow-2xl '>
             Kontakt
           </button>
         </a>
