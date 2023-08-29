7862c25cc8e78908e68b9cb1ded1b0d4940ff01e
Franciszek Sikora <sikorafranek@gmail.com>
2023-08-29 02:41:31 +0200

    fixed deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,44 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import NetlifyCMS from 'astro-netlify-cms';
+
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), NetlifyCMS({
-    config: {
-      backend: {
-        name: 'git-gateway',
-        branch: 'main'
-      },
-      collections: [
-        {
-          label: 'Posts',
-          name: 'posts',
-          slug: '{{postTitle}}',
-          folder: 'src/pages/posts',
-          create: true,
-          delete: true,
-          fields: [
-            {
-              label: 'Post Title',
-              name: 'postTitle',
-              widget: 'string',
-            },
-            {
-              label: 'Post Description',
-              name: 'postDescription',
-              widget: 'string'
-            },
-            {
-              label: 'Post URL',
-              name: 'postUrl',
-              widget: 'string'
-            }
-          ]
-        }
-      ]
-    }
-  })]
+  integrations: [tailwind(), react()]
 });
